c505cbcb50372f2c8f9402ebe2a7016be673b144
Your Name <you@example.com>
2019-11-07 11:38:29 -0300

    Atualizando Front

--- src/gerente/mainComponents/qtdabastecimentotela/qtdabastecimento.js
+++ src/gerente/mainComponents/qtdabastecimentotela/qtdabastecimento.js
@@ -1,10 +1,13 @@
 import React from "react";
+import {InputComponent} from "../component";
 
 class QtdAbastecimento extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            combustiveis : []
+            combustiveis : [],
+            mes : undefined,
+            ano : undefined
         }
     }
     getCombustivelItem(combustiveis){
@@ -20,19 +23,33 @@ class QtdAbastecimento extends React.Component{
         })
     }
     getCombustiveis(){
-        fetch("http://localhost:8080/combustivel/query10?id=1", {method : "GET"})
+        let requestParams = new URLSearchParams();
+        requestParams.append("id", 1);
+        requestParams.append("mes", this.state.mes);
+        requestParams.append("ano", this.state.ano);
+        fetch("http://localhost:8080/combustivel/query10?"+requestParams.toString(), {method : "GET"})
             .then((response)=>response.json())
             .then((data)=>{this.getCombustivelItem(data)})
             .catch(()=>{window.alert("Fetch Error")});  
     }
-    componentDidMount(){
-        this.getCombustiveis();
+    setMonth(event){
+        let data = event.target.value.split("-");
+        this.setState({
+            mes : data[0],
+            ano : data[1]
+        });
     }
     render(){
         return(
             <div className="tableContainer">
                 <div>
                     <h2>Abastecimentos por Combustível</h2>
+                </div>
+                <div>
+                    <InputComponent type="month" text="Mês e Ano(MM-YYYY)" onChange={(e)=>this.setMonth(e)}/>
+                </div>
+                <div>
+                    <input value="Exibir" type="submit" onClick={(e)=>{this.getCombustiveis()}}/>
                 </div>
                 <div>
                     <table>
